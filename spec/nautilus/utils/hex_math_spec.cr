89189049f455c0ab854be66a24522755294d7151
Ferhat Ziba <fero.ziba@gmail.com>
2021-02-01 02:54:16 +0100

    add Number Type based on hex

--- spec/nautilus/utils/hex_math_spec.cr
+++ spec/nautilus/utils/hex_math_spec.cr
@@ -1,22 +1,20 @@
 describe Nautilus::Utils::HexMath do
   it "check addition" do
-    a = "1"
-    b = "2"
-    Nautilus::Utils::HexMath.add(a,b).should eq "3"
-    Nautilus::Utils::HexMath.add(b,a).should eq "3"
+    a = "01"
+    b = "02"
+    Nautilus::Utils::HexMath.add(a,b).should eq "03"
+    Nautilus::Utils::HexMath.add(b,a).should eq "03"
 
-    c = "A"
-    Nautilus::Utils::HexMath.add(a,c).should eq "B"
-    Nautilus::Utils::HexMath.add(c,a).should eq "B"
-    d = "F"
+    c = "0A"
+    Nautilus::Utils::HexMath.add(a,c).should eq "0b"
+    Nautilus::Utils::HexMath.add(c,a).should eq "0b"
+    d = "0F"
     Nautilus::Utils::HexMath.add(c,d).should eq "19"
     Nautilus::Utils::HexMath.add(d,c).should eq "19"
 
     e = "19"
-    Nautilus::Utils::HexMath.add(e,b).should eq "1B"
-    puts b
-    puts e
-    Nautilus::Utils::HexMath.add(b,e).should eq "1B"
+    Nautilus::Utils::HexMath.add(e,b).should eq "1b"
+    Nautilus::Utils::HexMath.add(b,e).should eq "1b"
 
   end
 end
