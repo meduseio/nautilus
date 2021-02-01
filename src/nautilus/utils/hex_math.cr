3f14c6de429177849f9e6ad0e6d4f9ae9d86aeef
Ferhat Ziba <fero.ziba@gmail.com>
2021-02-01 02:54:01 +0100

    refactor math

--- src/nautilus/utils/hex_math.cr
+++ src/nautilus/utils/hex_math.cr
@@ -1,59 +1,88 @@
 module Nautilus
   module Utils
     class HexMath
-      HEX_VALUE_OF_DEC  = {
-        '0' => 0, '1' => 1,
-        '2' => 2, '3' => 3,
-        '4' => 4, '5' => 5,
-        '6' => 6, '7' => 7,
-        '8' => 8, '9' => 9,
-        'A' => 10, 'B' => 11,
-        'C' => 12, 'D' => 13,
-        'E' => 14, 'F' => 15
-      }
-      DEC_VALUE_OF_HEX = {
-        0 => '0', 1 => '1',
-        2 => '2', 3 => '3',
-        4 => '4', 5 => '5',
-        6 => '6', 7 => '7',
-        8 => '8', 9 => '9',
-        10 => 'A', 11 => 'B',
-        12 => 'C', 13 => 'D',
-        14 => 'E', 15 => 'F'
-      }
 
-      def self.add(a : String, b : String) : String
-        if (a.size() < b.size())
-          t = a
-          a = b
-          b = t
+      private def self._swap(a: UInt8, b: UInt8 8) : (UInt8, UInt8)
+        (b, a)
+      end
+
+      private def self._swap(a : Bytes, b : Bytes) : (Bytes, Bytes)
+        (b, a)
+      end
+
+      private def self._sub(a: UInt8, b: UInt8 8) : (UInt8, UInt8)
+        carry = 0
+        if (a < b )
+          carry = 1
+          a,b = _swap(a,b)
         end
-        m = HEX_VALUE_OF_DEC
-        k = DEC_VALUE_OF_HEX
+        diff = a - b
+        (diff, carry)
+      end
+
+      def self.abs(a : Bytes, b : Bytes) : Bytes
+        _swap(a,b) if (a.size() < b.size())
         l1 = a.size
         l2 = b.size
-        i = l1 - 1;
-        j = l2 - 1;
-        carry = 0
-        sum = 0
-        builder = String::Builder.new
+        carry : UInt8 = 0
+        memory = Array(UInt8).new
         while (j >= 0)
-          sum = m[a[i]] + m[b[j]] + carry;
-          addition_bit = k[sum % 16];
-          builder << addition_bit;
-          carry = (sum / 16).to_i;
+          sum, carry = _sub(a[i], b[j])
+          memory << (sum % 256)
           i -= 1
           j -= 1
         end
         while (i >= 0)
-          sum = m[a[i]] + carry;
-          addition_bit = k[sum % 16];
-          builder << addition_bit;
-          carry = (sum / 16).to_i;
+          sum, carry = _sub(a[i], b[j])
+          memory << (sum % 256)
           i -= 1
         end
-        builder << k[carry] if (carry > 0)
-        builder.to_s.reverse
+      end
+      def self.add(a : Bytes, b : Bytes) : Bytes
+        _swap(a,b) if (a.size() < b.size())
+        l1 = a.size
+        l2 = b.size
+        i = l1 - 1;
+        j = l2 - 1;
+        carry : UInt8 = 0
+        memory = Array(UInt8).new
+        while (j >= 0)
+          sum = a[i] + b[j] + carry
+          memory << (sum % 256)
+          carry = (sum / 256).to_u8
+          i -= 1
+          j -= 1
+        end
+        while (i >= 0)
+          sum = a[i] + carry
+          memory << (sum % 256)
+          carry = (sum / 256).to_u8
+          i -= 1
+        end
+        memory << carry.to_u8 if (carry > 0)
+        BytesToolBox.convert_array_to_bytes(memory.reverse)
+      end
+
+      def sub(a : Bytes, b : Bytes)  : Bytes
+        if(b.size() > a.size() || b[0] > a[0])
+          raise "Second Value is bigger"
+        end
+        l1 = a.size
+        l2 = b.size
+        i = l1 - 1;
+        j = l2 - 1;
+        carry : UInt8 = 0
+        memory = Array(UInt8).new
+        while (j >= 0)
+          subt = 0.to_u8
+          if((a[i] + carry) => b [i])
+            subt = a[i] - b [i]
+          else()
+        end
+      end
+
+      def self.add(a : String, b : String) : String
+        add(a.downcase.hexbytes,b.downcase.hexbytes).hexstring
       end
     end
   end
